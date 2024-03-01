4a1b4cde0319ffb3b9e1ca02fc1562ff0bea341c
Pawel80256 <paweuq2@yahoo.pl>
2024-03-01 20:39:18 +0100

    data-manager init

---
+++ noaa-data-manager/src/main/java/com/example/noaadatamanager/NoaaDataManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.noaadatamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoaaDataManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoaaDataManagerApplication.class, args);
+	}
+
+}
---
+++ noaa-data-manager/src/test/java/com/example/noaadatamanager/NoaaDataManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noaadatamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoaaDataManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
