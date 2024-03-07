83922d5293498166f1c9372e07d00a2273f2e8fd
Pawel80256 <paweuq2@yahoo.pl>
2024-03-07 22:18:43 +0100

    case 5

--- noaa-data-manager/src/main/java/com/example/noaadatamanager/aspects/AuthorizationAspect.java
+++ noaa-data-manager/src/main/java/com/example/noaadatamanager/aspects/AuthorizationAspect.java
@@ -1,17 +1,21 @@
 package com.example.noaadatamanager.aspects;
 
 import com.example.noaadatamanager.annotations.RequestAuthorization;
+import com.example.noaadatamanager.models.Role;
+import com.example.noaadatamanager.repository.RoleRepository;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.hibernate.annotations.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -23,9 +27,16 @@ import java.util.List;
 
 
 @Aspect
+@Service
 public class AuthorizationAspect {
     private static final String SECRET_KEY = "mojBardzoTajnyKluczDoGenerowaniaTokenowJWT...";
     private final SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+
+    private RoleRepository roleRepository;
+
+    public AuthorizationAspect(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
 
     @Pointcut("@annotation(com.example.noaadatamanager.annotations.RequestAuthorization)")
     public void authorizedMethods() {}
@@ -34,6 +45,7 @@ public class AuthorizationAspect {
     public void test(JoinPoint joinPoint) {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
         String authorizationHeader = request.getHeader("Authorization");
+
         if (authorizationHeader != null && !authorizationHeader.isBlank() && authorizationHeader.startsWith("Bearer ")) {
             String token = authorizationHeader.substring(7);
 
@@ -46,12 +58,16 @@ public class AuthorizationAspect {
             MethodSignature signature = (MethodSignature) joinPoint.getSignature();
             Method method = signature.getMethod();
             RequestAuthorization requestAuthorization = method.getAnnotation(RequestAuthorization.class);
-            List<String> requiredRoles = Arrays.stream(requestAuthorization.roles()).toList();
-            List<String> tokenRoles = (List<String>) claims.get("roles");
 
+            List<String> requiredRolesIds = Arrays.stream(requestAuthorization.roles()).toList();
+            List<String> tokenRolesIds = (List<String>) claims.get("roles");
 
-            System.out.println("Wymagane role: " + String.join(", ", requiredRoles));
-            System.out.println("Role użytkownika wykonującego requestt: " + String.join(", ", tokenRoles));
+            List<Role> requiredRoles = roleRepository.findAllById(requiredRolesIds);
+            List<Role> tokenRoles = roleRepository.findAllById(tokenRolesIds);
+
+            System.out.println("Wymagane role: " + String.join(", ", requiredRoles.stream().map(Role::getId).toList()));
+            System.out.println("Role użytkownika wykonującego requestt: " + String.join(", ", tokenRoles.stream().map(Role::getId).toList()));
         }
+
     }
 }
