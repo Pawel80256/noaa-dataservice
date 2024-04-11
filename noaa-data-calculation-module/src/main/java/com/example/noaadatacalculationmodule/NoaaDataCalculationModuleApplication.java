7bdfef06afc4b1ee58f6946e7d16f2af9553a8ce
Pawel80256 <paweuq2@yahoo.pl>
2024-04-11 12:16:03 +0200

    initialized calculation module

---
+++ noaa-data-calculation-module/src/main/java/com/example/noaadatacalculationmodule/NoaaDataCalculationModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.noaadatacalculationmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoaaDataCalculationModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoaaDataCalculationModuleApplication.class, args);
+	}
+
+}
---
+++ noaa-data-calculation-module/src/test/java/com/example/noaadatacalculationmodule/NoaaDataCalculationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noaadatacalculationmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoaaDataCalculationModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
